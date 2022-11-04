ab083dfdb8ad2f32e7e91ed79e288067d8601ed3
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-11-04 13:59:10 -0600

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,3 +2,6 @@ let containerE1 = document.getElementById("container")
 let inputE1 = document.getElementById("input")
 let inputE2 = document.getElementById("input2")
 
+let Names = ["placehold","placehold2","placehold3"] 
+
+containerE1.innerHTML = "<div>" + Names[2] + "</div>"
