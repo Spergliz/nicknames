095efdd7d661bc8508f256cea56399bdce120f04
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-11-04 14:49:08 -0600

    d

--- Main.js
+++ Main.js
@@ -1,7 +1,13 @@
-let containerE1 = document.getElementById("container")
-let inputE1 = document.getElementById("input")
-let inputE2 = document.getElementById("input2")
+let containerE1 = document.getElementById("container");
+let inputE1 = document.getElementById("input");
+let inputE2 = document.getElementById("input2");
+document.getElementById("button").addEventListener("click",gay);
+let Names = ["placehold", "placehold2", "placehold3"];
 
-let Names = ["placehold","placehold2","placehold3"] 
-
-containerE1.innerHTML = "<div>" + Names[2] + "</div>"
+function randomint(low, high) {
+  return Math.floor(Math.random() * (high - low) + low);
+}
+function gay(){
+containerE1.innerHTML = "<div>" + Names[randomint(0, 3)] + "</div>";
+}
+//
