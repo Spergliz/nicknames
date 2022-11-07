fd818e5be1169283e1730b060eba053887171775
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-11-07 14:48:41 -0700

    ghdr

--- Main.js
+++ Main.js
@@ -1,12 +1,26 @@
 let containerE1 = document.getElementById("container");
+let containerE2 = document.getElementById("container2");
 let inputE1 = document.getElementById("input");
 let inputE2 = document.getElementById("input2");
-document.getElementById("button").addEventListener("click",gay);
-let Names = ["test", "test2", "test3"];
+document.getElementById("button").addEventListener("click", wORK);
+document.getElementById("button2").addEventListener("click", wORK2)
+
+let Names = ["ubuntu user", "linux user", "Arch user","Mint user"];
 
 function randomint(low, high) {
   return Math.floor(Math.random() * (high - low) + low);
 }
-function gay(){
-containerE1.innerHTML = "<div>" + Names[randomint(0, 3)] + "</div>";
+
+function wORK() {
+  let nickname = Names[randomint(0, 3)];
+  let firstname = inputE1.value
+  let lastname = inputE2.value
+
+  containerE1.innerHTML = `${firstname} "${nickname}" ${lastname}`
+}
+
+function wORK2 (){
+  for(let i = 0; i< Names.length; i++){
+  
+  }
 }
